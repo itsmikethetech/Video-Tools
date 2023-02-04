af8d6317faab2953d96e12051ae3a7a331f6e0c6
mikethetech2 <contact@mikethetech.com>
2023-02-04 12:50:22 -0800

    Add project files.

---
+++ VideoConverter/MainWindow.xaml.vb
@@ -0,0 +1,14 @@
+﻿Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        System.Diagnostics.Process.Start("convert.bat")
+    End Sub
+
+    Private Sub Button_Click_1(sender As Object, e As RoutedEventArgs)
+        System.Diagnostics.Process.Start("transcode.bat")
+    End Sub
+
+    Private Sub ProgressBar_ValueChanged(sender As Object, e As RoutedPropertyChangedEventArgs(Of T))
+
+    End Sub
+End Class
+
